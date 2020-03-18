051df6711c5afd6a368544f7539ced7e25cf4b2e
Florentín Pérez González <alu0101100654@ull.edu.es>
2020-03-18 21:38:53 +0000

    Travis

--- test/example.js
+++ test/example.js
@@ -5,4 +5,9 @@ describe('Operations', function () {
   it ('Should be a function', function () {
     chai.expect(operations).to.be.a('function');
   });
+  it ('Should works fine', function() {
+    chai.expect(operations(0,2,3)).to.be.equal(5);
+    chai.expect(operations(1,2,3)).to.be.equal(-1);
+    chai.expect(operations(2,2,3)).to.be.equal(6);
+  });
 });
